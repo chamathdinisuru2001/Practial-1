9a2d15afc96c411617d24c274919bd0f013d365a
chamathdinisuru2001 <108864016+chamathdinisuru2001@users.noreply.github.com>
2024-06-18 08:47:55 +0530

    Update practical1.scala

--- practical1.scala
+++ practical1.scala
@@ -7,7 +7,7 @@ def volume(r:Double):Double=4/3*math.Pi*r*r*r
 def bookPrice(x:Int):Double=x*24.95
 def dicount(amount:Double):Double=amount*0.4
 def shipcost(n:Int):Double=3*n+(n-50)*0.75
-def totalCost(n:Int):Double=bookPrice(n)-dicount(n)+shipcost(n)
+def totalCost(n:Int):Double=bookPrice(n)-dicount(bookPrice(n))+shipcost(n)
 
 def easy(x:Int):Double=x*8
 def tempo(x:Int):Double=x*7
